226120596f8bf3cf35acc319e229ae9218d920ec
flapperkewiet <r0737258@student.thomasmore.be>
2021-03-08 21:47:54 +0100

    added decompression benchmarks

--- NebulaWorld/CompressionBenchmark.cs
+++ NebulaWorld/CompressionBenchmark.cs
@@ -12,23 +12,48 @@ namespace NebulaWorld
 {
     public static class CompressionBenchmark
     {
-        public delegate long Benchmark();
-        public const int iterations = 10;
+        public delegate long Benchmark(out byte[] data);
+        public delegate void DecompressionBenchmark(byte[] data);
+
+        public const int iterations = 15;
 
         public static void RunAllBenchmarks()
         {
-            RunBenchmark(LZ4AltFastBuffered4K);
-            RunBenchmark(LZ4AltFastBuffered8K);
-            return;
+            
             RunBenchmark(RawBinary);
             RunBenchmark(BuiltInGZip);
             RunBenchmark(BuiltInGZipBuffered4K);
             RunBenchmark(BuiltInGZipBuffered8K);
             RunBenchmark(LZ4);
-            RunBenchmark(LZ4HighCompression);
             RunBenchmark(LZ4AltFast);
-            RunBenchmark(LZ4AltHighCompression);
-            RunBenchmark(XZ);
+            RunBenchmark(LZ4AltFastBuffered4K);
+            RunBenchmark(LZ4AltFastBuffered8K);
+
+            
+            byte[] data;
+            RawBinary(out data);
+            RunDecompressionBenchmark(RawBinaryDecompression, data);
+
+            BuiltInGZip(out data);
+            RunDecompressionBenchmark(BuiltInGZipDecompression, data);
+
+            BuiltInGZipBuffered4K(out data);
+            RunDecompressionBenchmark(BuiltInGZipBuffered4KDecompression, data);
+
+            BuiltInGZipBuffered8K(out data);
+            RunDecompressionBenchmark(BuiltInGZipBuffered8KDecompression, data);
+
+            LZ4(out data);
+            RunDecompressionBenchmark(LZ4Decompression, data);
+
+            LZ4AltFast(out data);
+            RunDecompressionBenchmark(LZ4AltFastDecompression, data);
+
+            LZ4AltFastBuffered4K(out data);
+            RunDecompressionBenchmark(LZ4AltFastBuffered4KDecompression, data);
+
+            LZ4AltFastBuffered8K(out data);
+            RunDecompressionBenchmark(LZ4AltFastBuffered8KDecompression, data);
         }
 
         public static void RunBenchmark(Benchmark function)
@@ -43,9 +68,11 @@ namespace NebulaWorld
                 {
                     sw.Reset();
                     sw.Start();
-                    bytesInStream.Add(function());
+                    byte[] data;
+                    bytesInStream.Add(function(out data));
                     sw.Stop();
-                    timesPerIteration.Add(sw.Elapsed.TotalMilliseconds);
+                    if (i >= 5)
+                        timesPerIteration.Add(sw.Elapsed.TotalMilliseconds);
                 }
                 Log.Info(function.Method.Name);
                 Log.Info($"\t (min, max, avg) time in ms ({(int)timesPerIteration.Min()}, {(int)timesPerIteration.Max()}, {(int)timesPerIteration.Average()})");
@@ -61,7 +88,46 @@ namespace NebulaWorld
             }
         }
 
-        public static long RawBinary()
+        public static void RunDecompressionBenchmark(DecompressionBenchmark function, byte[] data)
+        {
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                List<double> timesPerIteration = new List<double>();
+                Log.Info("Compressed size: " + data.Length);
+                for (int i = 0; i < iterations; i++)
+                {
+                    sw.Reset();
+                    sw.Start();
+                    function(data);
+                    sw.Stop();
+                    if (i >= 5)
+                        timesPerIteration.Add(sw.Elapsed.TotalMilliseconds);
+                }
+                Log.Info(function.Method.Name);
+                Log.Info($"\t (min, max, avg) time in ms ({(int)timesPerIteration.Min()}, {(int)timesPerIteration.Max()}, {(int)timesPerIteration.Average()})");
+            }
+            catch (System.Exception e)
+            {
+                Log.Error(function.Method.Name);
+                Log.Error(e.Message);
+                Log.Error(e.StackTrace);
+            }
+        }
+
+        public static void RawBinaryDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (BinaryReader br = new BinaryReader(ms))
+                {
+                    GameMain.data.Import(br);
+                }
+            }
+        }
+
+        public static long RawBinary(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -69,11 +135,26 @@ namespace NebulaWorld
                 {
                     GameMain.data.Export(bw);
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long BuiltInGZip()
+        public static void BuiltInGZipDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (GZipStream gzip = new GZipStream(ms, CompressionMode.Decompress))
+                {
+                    using (BinaryReader br = new BinaryReader(gzip))
+                    {
+                        GameMain.data.Import(br);
+                    }
+                }
+            }
+        }
+
+        public static long BuiltInGZip(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -84,11 +165,28 @@ namespace NebulaWorld
                         GameMain.data.Export(bw);
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long BuiltInGZipBuffered4K()
+        public static void BuiltInGZipBuffered4KDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (GZipStream gzip = new GZipStream(ms, CompressionMode.Decompress, true))
+                {
+                    using (BufferedStream bs = new BufferedStream(gzip, 4096))
+                    {
+                        using (BinaryReader br = new BinaryReader(bs))
+                        {
+                            GameMain.data.Import(br);
+                        }
+                    }
+                }
+            }
+        }
+        public static long BuiltInGZipBuffered4K(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -102,11 +200,29 @@ namespace NebulaWorld
                         }
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long BuiltInGZipBuffered8K()
+        public static void BuiltInGZipBuffered8KDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (GZipStream gzip = new GZipStream(ms, CompressionMode.Decompress, true))
+                {
+                    using (BufferedStream bs = new BufferedStream(gzip, 8192))
+                    {
+                        using (BinaryReader br = new BinaryReader(bs))
+                        {
+                            GameMain.data.Import(br);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static long BuiltInGZipBuffered8K(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -120,11 +236,26 @@ namespace NebulaWorld
                         }
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long LZ4()
+        public static void LZ4Decompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (LZ4Stream lz4 = new LZ4Stream(ms, CompressionMode.Decompress, LZ4StreamFlags.Default))
+                {
+                    using (BinaryReader br = new BinaryReader(lz4))
+                    {
+                        GameMain.data.Import(br);
+                    }
+                }
+            }
+        }
+
+        public static long LZ4(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -135,25 +266,26 @@ namespace NebulaWorld
                         GameMain.data.Export(bw);
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long LZ4HighCompression()
+        public static void LZ4AltFastDecompression(byte[] data)
         {
-            using (MemoryStream ms = new MemoryStream())
+            using (MemoryStream ms = new MemoryStream(data))
             {
-                using (LZ4Stream lz4 = new LZ4Stream(ms, CompressionMode.Compress, LZ4StreamFlags.HighCompression))
+                using (Lz4DecompressionStream lz4 = new Lz4DecompressionStream(ms))
                 {
-                    using (BinaryWriter bw = new BinaryWriter(lz4))
+                    using (BinaryReader br = new BinaryReader(lz4))
                     {
-                        GameMain.data.Export(bw);
+                        GameMain.data.Import(br);
                     }
                 }
-                return ms.ToArray().Length;
             }
         }
-        public static long LZ4AltFast()
+
+        public static long LZ4AltFast(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -164,11 +296,30 @@ namespace NebulaWorld
                         GameMain.data.Export(bw);
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long LZ4AltFastBuffered4K()
+
+        public static void LZ4AltFastBuffered4KDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (Lz4DecompressionStream lz4 = new Lz4DecompressionStream(ms))
+                {
+                    using (BufferedStream bs = new BufferedStream(lz4, 4096))
+                    {
+                        using (BinaryReader br = new BinaryReader(bs))
+                        {
+                            GameMain.data.Import(br);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static long LZ4AltFastBuffered4K(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -182,11 +333,29 @@ namespace NebulaWorld
                         }
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long LZ4AltFastBuffered8K()
+        public static void LZ4AltFastBuffered8KDecompression(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (Lz4DecompressionStream lz4 = new Lz4DecompressionStream(ms))
+                {
+                    using (BufferedStream bs = new BufferedStream(lz4, 8192))
+                    {
+                        using (BinaryReader br = new BinaryReader(bs))
+                        {
+                            GameMain.data.Import(br);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static long LZ4AltFastBuffered8K(out byte[] data)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -200,37 +369,10 @@ namespace NebulaWorld
                         }
                     }
                 }
+                data = ms.ToArray();
                 return ms.ToArray().Length;
             }
         }
 
-        public static long LZ4AltHighCompression()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (Lz4CompressionStream lz4 = new Lz4CompressionStream(ms, Lz4Mode.HighCompression))
-                {
-                    using (BinaryWriter bw = new BinaryWriter(lz4))
-                    {
-                        GameMain.data.Export(bw);
-                    }
-                }
-                return ms.ToArray().Length;
-            }
-        }
-        public static long XZ()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (XZInputStream xz = new XZInputStream(ms))
-                {
-                    using (BinaryWriter bw = new BinaryWriter(xz))
-                    {
-                        GameMain.data.Export(bw);
-                    }
-                }
-                return ms.ToArray().Length;
-            }
-        }
     }
 }
